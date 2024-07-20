e74c405e0a0a824d62d5eabd407ba4f8afd20c64
tphuc263 <tphuc.ptit@gmail.com>
2024-07-20 19:55:11 +0700

    Create question_model.py

---
+++ question_model.py
@@ -0,0 +1,8 @@
+class Question():
+    def __init__(self, category, type, difficulty, question, correct_answer, incorrect_answer):
+        self.category = category
+        self.type = type
+        self.difficulty = difficulty
+        self.question = question
+        self.correct_answer = correct_answer
+        self.incorrect_answer = incorrect_answer
